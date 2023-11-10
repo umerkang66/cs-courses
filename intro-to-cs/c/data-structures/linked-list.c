96f32c53a150df68e76ea4aacd8955d520505f71
umerkang66 <umerkang66@gmail.com>
2023-11-10 05:51:41 +0000

    add linked list

---
+++ intro-to-cs/c/data-structures/linked-list.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// we have to add a name here as well, to get accessed in line 10, then rename this structure
+typedef struct node
+{
+    int number;
+    // if next element is itself a node,
+    // so next is the pointer of that node
+    // this would be the address of a node
+    struct node *next;
+}
+node;
+
+void printList(node *list);
+
+int main(void)
+{
+    node *list = NULL;
+
+    // create a linkedList from 1 to n
+    for (int i = 1; i <= 20; i++)
+    {
+        node *n = malloc(sizeof(node));
+        n->number = i;
+        n->next = list;
+        list = n;
+    }
+
+    printList(list);
+}
+
+void printList(node *list)
+{
+    node *currentNode = list;
+    while (1)
+    {
+        if (currentNode->next == NULL)
+        {
+            printf("%i\n", currentNode->number);
+            break;
+        }
+        printf("%i -> ", currentNode->number);
+        currentNode = currentNode->next;
+    }
+}
