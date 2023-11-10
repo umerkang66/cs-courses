5d9e3dc5ecb565564c0381dc61cf1145fd711e67
umerkang66 <umerkang66@gmail.com>
2023-11-10 06:23:47 +0000

    update linked-list

--- intro-to-cs/c/data-structures/linked-list.c
+++ intro-to-cs/c/data-structures/linked-list.c
@@ -13,25 +13,27 @@ typedef struct node
 node;
 
 void printList(node *list);
+void freeList(node *list);
 
+// we can't use binary search on linked list, because elements are not consecutive in memory
 int main(void)
 {
     node *list = NULL;
-
     // create a linkedList from 1 to n
     for (int i = 1; i <= 20; i++)
     {
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
+            // BUG: if the malloc error happens in other than first iteration of loop, then previous mallocated would be memory-leaked, because we just return it.
             return 1;
         }
         n->number = i;
         n->next = list;
         list = n;
     }
-
     printList(list);
+    freeList(list);
 }
 
 void printList(node *list)
@@ -48,5 +50,16 @@ void printList(node *list)
             printf("%i -> ", currentNode->number);
         }
         currentNode = currentNode->next;
+    }
+}
+
+void freeList(node *list)
+{
+    node *currentNode = list;
+    while (currentNode != NULL)
+    {
+        node *tempNext = currentNode->next;
+        free(currentNode);
+        currentNode = tempNext;
     }
 }
