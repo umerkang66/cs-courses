5ede2cf0915e1864ba27603f87ccccc06793952f
Muhammad Umer <umerkang66@gmail.com>
2023-12-26 13:23:22 +0500

    add friend class

--- cpp/oop/friend_classes.cpp
+++ cpp/oop/friend_classes.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// forward declaration
+class Complex;
+
 class Calculator
 {
 public:
-    int add(int a, int b) { return a + b; }
-    Complex sumComplex(Complex a, Complex b)
-    {
-        Complex answer;
-        answer.setNumber(a.a + b.a, a.b + b.b);
-    }
+    Complex sumComplexComplete(Complex, Complex);
+    int sumRealPart(Complex, Complex);
+    int sumImagPart(Complex, Complex);
 };
 
 class Complex
@@ -19,7 +19,11 @@ private:
 
 public:
     // hey sumComplex of of Calculator class, get access to my private data
-    friend Complex Calculator::sumComplex(Complex a, Complex b);
+    // Individually declaring functions as FRIENDS
+    friend Complex Calculator::sumComplexComplete(Complex a, Complex b);
+    // Declaring the entire Calculator class
+    friend class Calculator;
+
     void setNumber(int n1, int n2)
     {
         a = n1;
@@ -31,8 +35,37 @@ public:
     }
 };
 
+Complex Calculator::sumComplexComplete(Complex a, Complex b)
+{
+    Complex answer;
+    answer.setNumber(a.a + b.a, a.b + b.b);
+    return answer;
+}
+// BELOW two functions will run, because we have made the whole class FRIEND CLASS
+int Calculator::sumRealPart(Complex a, Complex b)
+{
+    return a.a + b.a;
+}
+int Calculator::sumImagPart(Complex a, Complex b)
+{
+    return a.b + b.b;
+}
+
 int main(int argc, char const *argv[])
 {
+    Complex a, b, sum;
+    a.setNumber(4, 5);
+    b.setNumber(3, 9);
+
+    Calculator calculator;
+    sum = calculator.sumComplexComplete(a, b);
+
+    a.printNumber();
+    b.printNumber();
+    sum.printNumber();
+
+    cout << "The of real part: " << calculator.sumRealPart(a, b) << endl;
+    cout << "The of imaginary part: " << calculator.sumImagPart(a, b) << endl;
 
     return 0;
 }
