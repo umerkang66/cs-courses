421a0d6a6862d421ed8b9be95a296047edf4d801
Muhammad Umer <umerkang66@gmail.com>
2023-12-29 19:31:05 +0500

    update cafe_ms

--- cpp/basics/cafe_ms.cpp
+++ cpp/basics/cafe_ms.cpp
@@ -18,7 +18,7 @@ int main()
 	{
 		string spacedItem = items[i];
 		int itemLength = items[i].length();
-		int howManySpaces = i==length-1?24:25;
+		int howManySpaces = i == length - 1 ? 24 : 25;
 		for (int i = itemLength; i < howManySpaces; i++)
 		{
 			spacedItem += " ";
@@ -26,10 +26,10 @@ int main()
 		cout << i + 1 << ": " << spacedItem << ": " << prices[i] << " Rs." << endl;
 	}
 	cout << endl;
-	
+
 	char purchaseFurther;
 	int whichItem;
-	
+
 	cout << "Enter item no. to purchase: ";
 	cin >> whichItem;
 	while (whichItem < 1 || whichItem > 10)
@@ -37,14 +37,14 @@ int main()
 		cout << "Please enter no. between 1 and 10: ";
 		cin >> whichItem;
 	}
-	
+
 	// store the quantity in array at the index, and that index should also point to the item and price in other two arrays
 	cout << "How many items (quantity): ";
-	cin >> userPurchasedItems[whichItem-1];
-	
+	cin >> userPurchasedItems[whichItem - 1];
+
 	cout << "Do you want to purchase further? (y/n): ";
 	cin >> purchaseFurther;
-	
+
 	while (purchaseFurther != 'n' && purchaseFurther != 'N')
 	{
 		cout << "Enter item no. to purchase: ";
@@ -54,52 +54,50 @@ int main()
 			cout << "Please enter no. between 1 and 10: ";
 			cin >> whichItem;
 		}
-		
+
 		// store the quantity in array at the index, and that index should also point to the item and price in other two arrays
 		cout << "How many items (quantity): ";
-		cin >> userPurchasedItems[whichItem-1];
-	
+		cin >> userPurchasedItems[whichItem - 1];
+
 		cout << "Do you want to purchase further? (y/n): ";
 		cin >> purchaseFurther;
 	}
-	
+
 	int totalAmount = 0;
 	int orderNo = 1;
 	cout << endl;
 	cout << "Order No.     Item                     Quantity     Price" << endl;
-	for (int i = 0; i < 10; i++)                                 
+	for (int i = 0; i < 10; i++)
 	{
 		if (userPurchasedItems[i] != -1)
 		{
-			totalAmount += prices[i]*userPurchasedItems[i];	
-			
+			totalAmount += prices[i] * userPurchasedItems[i];
+
 			string spacedItem = items[i];
 			int itemLength = items[i].length();
-			int howManySpaces = orderNo==length?24:25;
+			int howManySpaces = orderNo == length ? 24 : 25;
 			for (int i = itemLength; i < howManySpaces; i++)
 			{
 				spacedItem += " ";
 			}
-			cout << orderNo << "             " << spacedItem << userPurchasedItems[i] << "            " << prices[i]*userPurchasedItems[i] << " Rs." << endl;
+			cout << orderNo << "             " << spacedItem << userPurchasedItems[i] << "            " << prices[i] * userPurchasedItems[i] << " Rs." << endl;
 			orderNo++;
 		}
 	}
-	
+
 	cout << endl;
-	
+
 	float tax = ((float)totalAmount) * 5.0 / 100.0;
 	float amountDue = totalAmount + tax;
 	float discount = totalAmount <= 1000 ? 0 : ((float)totalAmount) * 10.0 / 100.0;
-	
-	
+
 	cout << "Total Amount:    " << totalAmount << " Rs." << endl;
 	cout << "Tax (5%):        " << tax << " Rs." << endl;
 	cout << "Amount Due:      " << amountDue << " Rs." << endl;
 	cout << "Discount:        " << discount << " Rs." << endl;
 	cout << "Net Total:       " << amountDue - discount << " Rs." << endl;
-	
+
 	cout << endl;
 	cout << "Thank you for purchasing. Have a nice day.";
 	return 0;
 }
-
