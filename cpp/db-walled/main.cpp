fbccc241967602425996ebd45717fde02b944d5b
umerkang66 <umerkang66@gmail.com>
2024-01-03 11:09:00 +0000

    complete the mini project

--- cpp/db-walled/main.cpp
+++ cpp/db-walled/main.cpp
@@ -12,6 +12,15 @@ int main()
 
     int total_students_length = 100;
     char students[total_students_length][50];
+    char isPresent[total_students_length];
+    for (int i = 0; i < total_students_length; i++)
+    {
+        // entering the garbage value
+        // present ==> 1
+        // absent  ==> 0
+        isPresent[i] = -1;
+    }
+
     int counter = 0;
     char yesOrNo;
 
@@ -34,10 +43,42 @@ int main()
     cout << "Here are the names of students" << endl;
     cout << "If they are present enter 'p', otherwise 'a'" << endl;
 
-  
     for (int i = 0; *students[i] != '\0'; i++)
     {
-        cout << students[i] << endl;
+        cout << students[i] << " (p/a)? ";
+        char answer;
+        cin >> answer;
+        if (answer == 'p')
+        {
+            isPresent[i] = 1;
+        }
+        else if (answer == 'a')
+        {
+            isPresent[i] = 0;
+        }
+        else
+        {
+            isPresent[i] = -1;
+        }
+    }
+
+    // CONTROL TO STUDENT
+    for (int i = 0; *students[i] != '\0'; i++)
+    {
+        string message;
+        if (isPresent[i]==1)
+        {
+            message = "PRESENT";
+        }
+        else if (isPresent[i]==0)
+        {
+            message = "ABSENT";
+        }
+        else
+        {
+            message = "ATTENDANCE NOT MARKED";
+        }
+        cout << students[i] << " is " << message << endl;
     }
 
     return 0;
