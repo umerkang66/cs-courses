cd33bf9fc066ed883e558e9685387b527cad7f42
Muhammad Umer <umerkang66@gmail.com>
2024-01-04 16:56:13 +0500

    add inheritance

---
+++ cpp/oop/inheritance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+    int age;
+
+public:
+    Person(string _name, int _age)
+    {
+        name = _name;
+        age = _age;
+    }
+};
+
+class Student : Person
+{
+    string subject;
+
+public:
+    Student(string _name, int _age, string _subject) : Person(_name, _age)
+    {
+        subject = _subject;
+    }
+
+    void introduce(void)
+    {
+        cout << "My name is " << name << ". I am " << age << " years old, and studying " << subject << endl;
+    }
+};
+
+int main()
+{
+    Student umer("umer", 20, "computer science");
+    umer.introduce();
+
+    return 0;
+}
