c37f2b3ce118ef57a012bc2eab8dc75a4bcc99a5
Muhammad Umer <umerkang66@gmail.com>
2024-01-05 09:45:55 +0500

    add create_schema function

--- cpp/db-wallet/main.cpp
+++ cpp/db-wallet/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-void create_schema(string name, char values[3][15])
+void create_schema(string name, int length, string values[])
 {
+    string content = name + "\n";
+    for (int i = 0; i < length; i++)
+    {
+        content += (values[i] + ",");
+    }
+    content += "\n";
+    ofstream file("data.csv");
+    file << content;
+    file.close();
 }
 
 int main()
 {
-    char values[][15] = {"name", "phone_number", "balance"};
+    string name = "users";
+    int length = 3;
+    string values[length] = {"name", "phone_number", "balance"};
 
-    create_schema("users", values);
+    create_schema(name, length, values);
     return 0;
 }
