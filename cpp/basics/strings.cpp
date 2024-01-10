56ca43642a2943d6818c18842a3944a30de7515f
Muhammad Umer <umerkang66@gmail.com>
2024-01-10 11:31:25 +0500

    update strings

---
+++ cpp/basics/strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char s1[20] = "1";
+    int integer = stoi(s1);
+
+    cout << integer << endl;
+
+    return 0;
+}
