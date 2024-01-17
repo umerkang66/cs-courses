096b17dcad1574618afad1e35b5fcb616ca73d66
Muhammad Umer <umerkang66@gmail.com>
2024-01-17 17:06:26 +0500

    update main

--- cpp/db-wallet/main.cpp
+++ cpp/db-wallet/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-struct user
+struct User
 {
     char name[40];
-    char number[11];
+    char password[9]; // one for string terminator char
+    char number[12];
     int balance;
 };
+
+User signup_user();
+void save_user(User user);
 
 int main()
 {
@@ -22,6 +27,8 @@ int main()
         cin >> haveAccount;
     }
 
+    cin.ignore();
+
     if (haveAccount == 'y' || haveAccount == 'Y')
     {
         // USER WANTS TO SIGNIN
@@ -29,7 +36,47 @@ int main()
     else
     {
         // USER WANTS TO SIGNUP
+        User user = signup_user();
+        save_user(user);
     }
 
     return 0;
+}
+
+User signup_user()
+{
+    User user;
+
+    cout << "Enter your name: ";
+    cin.getline(user.name, 40);
+
+    cout << "Enter your number: ";
+    cin.getline(user.number, 40);
+
+    cout << "Enter your password 8 characters are allowed: ";
+    cin.getline(user.password, 40);
+    user.balance = 0;
+
+    return user;
+}
+
+void save_user(User user)
+{
+    // first check if the user table exists
+    ifstream input("data.csv");
+
+    string terminator = "---";
+    string complete_table = "";
+    string current_line;
+
+    while (getline(input, current_line) && current_line != terminator)
+    {
+        complete_table += current_line;
+        if (current_line != terminator)
+        {
+            complete_table += '\n';
+        }
+    }
+
+    cout << complete_table << endl;
 }
