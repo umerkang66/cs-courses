b677fedf708da35f3b87b79fb8575b77e6070ec1
Muhammad Umer <umerkang66@gmail.com>
2024-01-18 15:05:09 +0500

    add the functionality of getting all the users and transactions

--- cpp/db-wallet/main.cpp
+++ cpp/db-wallet/main.cpp
@@ -11,14 +11,28 @@ struct User
     int balance;
 };
 
+struct Transaction
+{
+    string from;
+    string to;
+    int amount;
+};
+
+const string terminator = "---";
+
 // APPLICATION FUNCTIONS
 void authenticate();
 User create_user();
 void signup_user();
-void save_user_to_file(User user);
+User *get_users();
+User *add_user_all_users(User user);
+void save_all_users(User *users);
+Transaction *get_transactions();
+
 // UTILITY FUNCTIONS
 string *split(string str, char separator);
 string get_all_tables();
+string get_table(string name);
 
 User current_user;
 
@@ -78,67 +92,142 @@ void signup_user()
     // create the user in memory
     User user = create_user();
     // save the user in the file
-    save_user_to_file(user);
+    User *users = add_user_all_users(user);
+    // saving all the users
+    save_all_users(users);
+    delete[] users;
+
     // set the current_user to this user
     current_user = user;
 }
 
-void save_user_to_file(User user)
+User *add_user_all_users(User user)
 {
-    // getting the table
-    string complete_table = get_all_tables();
-    cout << complete_table << endl;
-    string terminator = "---";
-    cout << complete_table << endl;
+    // getting the users
+    User *users = get_users();
+    // finding the length
+    int length = 0;
+    for (int i = 0; users[i].name != terminator; i++)
+    {
+        length++;
+    }
+    // make new array and copy all the elements in the new array
+    // +1 is for the new user
+    // and +1 is for the new terminator string
+    User *new_users = new User[length + 2];
+    int i;
+    for (i = 0; users[i].name != terminator; i++)
+    {
+        new_users[i].name = users[i].name;
+        new_users[i].password = users[i].password;
+        new_users[i].number = users[i].number;
+        new_users[i].balance = users[i].balance;
+    }
+    // adding the latest added user
+    new_users[i].name = user.name;
+    new_users[i].password = user.password;
+    new_users[i].number = user.number;
+    new_users[i].balance = user.balance;
 
-    // if user already exists update the user
-    // else add the user at the end of table
-    bool does_user_exists = false; // THIS IS TEMPORARY TRUE
-    // first create array by new line separator
-    string *lines = split(complete_table, '\n');
-    for (int i = 2; i < lines->size() - 2; i++)
+    new_users[++i].name = terminator;
+
+    // delete the old heap array
+    delete[] users;
+
+    return new_users;
+}
+
+void save_all_users(User *users)
+{
+    // first we have create all the rows
+    string rows = "users\nname,password,phone_number,balance";
+    for (int i = 0; users[i].name != terminator; i++)
+    {
+        string row = users[i].name + ',' + users[i].password + ',' + users[i].number + ',' + to_string(users[i].balance);
+        rows += '\n' + row;
+    }
+
+    cout << rows << endl;
+}
+
+// this will return the heap array, make sure to DEALLOCATE it
+User *get_users()
+{
+    string user_table = get_table("users");
+    int length = 0;
+
+    string *lines = split(user_table, '\n');
+    for (int i = 2; lines[i][0] != '\0'; i++)
+    {
+        length++;
+    }
+    // +1 is for the terminator character
+    User *users = new User[length + 1];
+    int users_counter = 0;
+
+    for (int i = 2; lines[i][0] != '\0'; i++)
     {
         // for every create an array by ',' separator
         string *fields = split(lines[i], ',');
-
-        // this field will be a number in database
-        if (fields[2] == user.number)
-        {
-            does_user_exists = true;
-        }
+        users[users_counter].name = fields[0];
+        users[users_counter].password = fields[1];
+        users[users_counter].number = fields[2];
+        users[users_counter].balance = stoi(fields[3]);
+        users_counter++;
 
         delete[] fields;
         fields = nullptr;
     }
 
+    users[users_counter].name = terminator;
+
     delete[] lines;
     lines = nullptr;
 
-    if (does_user_exists)
+    return users;
+}
+
+// this will return the heap array, make sure to DEALLOCATE it
+Transaction *get_transactions()
+{
+    string transaction_table = get_table("transactions");
+
+    int length = 0;
+    string *lines = split(transaction_table, '\n');
+    for (int i = 2; i < lines[i][0] != '\0'; i++)
     {
-        cout << "umer did this" << endl;
+        length++;
     }
-    else
+    // +1 is for the terminator character
+    Transaction *transactions = new Transaction[length + 1];
+    int transactions_counter = 0;
+
+    for (int i = 2; lines[i][0] != '\0'; i++)
     {
-        string row = user.name + ',' + user.password + ',' + user.number + ',' + to_string(user.balance) + '\n';
+        // for every create an array by ',' separator
+        string *fields = split(lines[i], ',');
+        transactions[transactions_counter].from = fields[0];
+        transactions[transactions_counter].to = fields[1];
+        transactions[transactions_counter].amount = stoi(fields[2]);
+        transactions_counter++;
 
-        complete_table += row;
+        delete[] fields;
+        fields = nullptr;
     }
 
-    complete_table += terminator;
+    transactions[transactions_counter].from = terminator;
 
-    // saving the row in the table
-    ofstream output("data.csv");
+    delete[] lines;
+    lines = nullptr;
 
-    output << complete_table;
-    output.close();
+    return transactions;
 }
 
 // UTILITY FUNCTIONS
 // This will split the string into an array by using the separator
 string *split(string str, char separator)
 {
-    int arr_length = 1;
+    int arr_length = 0;
     for (int i = 0; i < str.length(); i++)
     {
         if (str[i] == separator)
@@ -149,7 +238,9 @@ string *split(string str, char separator)
 
     // make an array of that length in the HEAP memory
     // make sure the DEALLOCATE this memory after using it
-    string *arr = new string[arr_length + 1];
+    // +2: +1 is for there is one more word than separator
+    // +1 is for terminator string character
+    string *arr = new string[arr_length + 2];
     int arr_counter = 0;
     string one_string = "";
     for (int i = 0; i < str.length(); i++)
@@ -167,6 +258,7 @@ string *split(string str, char separator)
         }
     }
     arr[arr_counter] = one_string;
+    arr[++arr_counter][0] = '\0';
 
     return arr;
 }
@@ -196,12 +288,90 @@ string get_table(string name)
     string all_tables = get_all_tables();
     string table_terminator = "---";
 
-    string table = "";
+    // to check if all the characters of name are in
+    // all_tables create an array
+    bool all_chars_of_name_in_all_tables[name.length()];
+    // initialize it with 0
+    for (int i = 0; i < name.length(); i++)
+    {
+        all_chars_of_name_in_all_tables[i] = 0;
+    }
+    int table_starting_index = -1;
+
     for (int i = 0; i < all_tables.length(); i++)
     {
         if (all_tables.length() > name.length())
         {
             // there is a chance that that table will exist
+            for (int j = 0; j < name.length(); j++)
+            {
+                if (all_tables[i + j] == name[j])
+                {
+                    all_chars_of_name_in_all_tables[j] = 1;
+                }
+            }
+
+            // check if the word is found
+            bool is_word_found = true;
+            for (int i = 0; i < name.length(); i++)
+            {
+                if (all_chars_of_name_in_all_tables[i] == 0)
+                {
+                    is_word_found = false;
+                }
+            }
+
+            // if all the characters are found
+            if (is_word_found)
+            {
+                // store the starting index
+                table_starting_index = i;
+                // and break the loop
+                break;
+            }
+            else
+            {
+                // reset the values
+                for (int i = 0; i < name.length(); i++)
+                {
+                    all_chars_of_name_in_all_tables[i] = 0;
+                }
+            }
         }
     }
+
+    if (table_starting_index == -1)
+    {
+        return "not found";
+    }
+
+    string table = "";
+    int i = table_starting_index;
+    // check for terminator
+    while (i < all_tables.length())
+    {
+        table += all_tables[i];
+        bool chars_of_terminator_found[3] = {0, 0, 0};
+        for (int j = i + 1; j <= i + 3; j++)
+        {
+            if (all_tables[j] == '-')
+            {
+                chars_of_terminator_found[j - i - 1] = 1;
+            }
+        }
+        bool is_found = true;
+        for (int i = 0; i < 3; i++)
+        {
+            if (chars_of_terminator_found[i] == 0)
+            {
+                is_found = false;
+            }
+        }
+        if (is_found)
+        {
+            break;
+        }
+        i++;
+    }
+    return table;
 }
