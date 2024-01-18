35fd04848d64f754b2f7263162edf26e830e9981
Muhammad Umer <umerkang66@gmail.com>
2024-01-18 09:31:35 +0500

    add split function

--- cpp/db-wallet/main.cpp
+++ cpp/db-wallet/main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 struct User
 {
-    char name[40];
-    char password[9]; // one for string terminator char
-    char number[12];
+    string name;
+    string password;
+    string number;
     int balance;
 };
 
-User signup_user();
+User create_user();
+void signup_user();
 void save_user(User user);
+
+User *current_user = nullptr;
 
 int main()
 {
@@ -36,33 +39,42 @@ int main()
     else
     {
         // USER WANTS TO SIGNUP
-        User user = signup_user();
-        save_user(user);
+        signup_user();
     }
 
     return 0;
 }
 
-User signup_user()
+User create_user()
 {
     User user;
 
     cout << "Enter your name: ";
-    cin.getline(user.name, 40);
+    getline(cin, user.name);
 
     cout << "Enter your number: ";
-    cin.getline(user.number, 40);
+    getline(cin, user.number);
 
     cout << "Enter your password 8 characters are allowed: ";
-    cin.getline(user.password, 40);
+    getline(cin, user.password);
     user.balance = 0;
 
     return user;
 }
 
+void signup_user()
+{
+    // create the user in memory
+    User user = create_user();
+    // save the user in the file
+    save_user(user);
+    // set the current_user to this user
+    *current_user = user;
+}
+
 void save_user(User user)
 {
-    // first check if the user table exists
+    // getting the table
     ifstream input("data.csv");
 
     string terminator = "---";
@@ -75,5 +87,46 @@ void save_user(User user)
         complete_table += '\n';
     }
 
-    cout << complete_table << endl;
+    input.close();
+
+    string row = user.name + ',' + user.password + ',' + user.number + ',' + to_string(user.balance) + '\n' + "---";
+
+    complete_table += row;
+
+    // saving the row in the table
+    ofstream output("data.csv");
+    output << complete_table;
+    output.close();
+}
+
+// UTILITY FUNCTIONS
+// This will split the string into an array by using the separator
+string *split(string str, char separator)
+{
+    int arr_length = 1;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == separator)
+        {
+            arr_length++;
+        }
+    }
+
+    // make an array of that length in the HEAP memory
+    // make sure the DEALLOCATE this memory after using it
+    string *arr = new string[arr_length];
+    int arr_counter = 0;
+    string one_string = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == separator)
+        {
+            arr[arr_counter] = one_string;
+            one_string = "";
+        }
+        one_string += str[i];
+    }
+    arr[arr_counter] = one_string;
+
+    return arr;
 }
