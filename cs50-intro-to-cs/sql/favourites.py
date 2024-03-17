011308ce63fc6ec1a61c7c12c03354b790ff48eb
Muhammad Umer <umerkang66@gmail.com>
2024-03-17 13:18:15 +0500

    remove unnecessary files

--- cs50-intro-to-cs/sql/favourites.py
+++
@@ -1,9 +0,0 @@
-from cs50 import SQL
-
-db = SQL("sqlite:///favourites.db")
-
-favourite = input("Favourite: ")
-
-rows = db.execute("SELECT COUNT(*) AS n FROM favourites WHERE problem = ?", favourite)
-
-print(rows[0]["n"])
