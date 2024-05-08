bb68639ca047beab0b43205fda5a8178ccf9d80c
Muhammad Umer <umerkang66@gmail.com>
2024-05-08 17:53:02 +0500

    start implementing the tree

--- dsa-abdul-bari/trees/main.cpp
+++ dsa-abdul-bari/trees/main.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 struct Node
 {
+  int data;
+  Node *left = nullptr;
+  Node *right = nullptr;
+
+  Node(int data) : data(data) {}
+};
+
+class Tree
+{
+  Node *root;
+
+public:
+  Tree()
+  {
+  }
 };
 
 int main()
