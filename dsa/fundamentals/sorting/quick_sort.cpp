2606eac1783d7165886482e53a51362240db3153
Muhammad Umer <umerkang66@gmail.com>
2024-07-18 11:59:27 +0500

    implement quick sort

--- dsa/fundamentals/sorting/quick_sort.cpp
+++ dsa/fundamentals/sorting/quick_sort.cpp
@@ -8,8 +8,41 @@ void swap(int *nums, int first, int second)
   nums[second] = temp;
 }
 
-void sort(int *nums, int n)
+// returns the index of pivot after moving to the right place
+int get_partition(int *nums, int start, int end)
 {
+  int i = start;
+  int j = end - 1;
+  int pivot = end;
+
+  while (i <= j)
+  {
+    while (i <= j && nums[i] < nums[pivot])
+      i++;
+    while (i <= j && nums[j] > nums[pivot])
+      j--;
+
+    if (i < j)
+    {
+      swap(nums, i, j);
+      i++;
+      j--;
+    }
+  }
+
+  swap(nums, i, pivot);
+  return i;
+}
+
+void sort(int *nums, int start, int end)
+{
+  if (start >= end)
+    return;
+
+  int pivot = get_partition(nums, start, end);
+
+  sort(nums, start, pivot - 1);
+  sort(nums, pivot + 1, end);
 }
 
 void display(int *nums, int n)
@@ -23,7 +56,7 @@ int main()
 {
   int nums[] = {5, 8, 4, 3, 9, 1};
   display(nums, 6);
-  sort(nums, 6);
+  sort(nums, 0, (sizeof(nums) / sizeof(nums[0])) - 1);
   display(nums, 6);
 
   return 0;
