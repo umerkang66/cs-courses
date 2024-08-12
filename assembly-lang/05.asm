3e8689a041687e7d0d2a0dfcfa242f0d17566a48
Muhammad Umer <umerkang66@gmail.com>
2024-08-12 16:33:50 +0500

    add 05.asm

---
+++ assembly-lang/05.asm
@@ -0,0 +1,22 @@
+[org 0x0100]
+
+  xor ax, ax ; set the ax = 0
+  ; store the num1 add at bx
+  mov bx, 0
+
+
+  outerloop: ; loop will run by using cx value
+    add ax, [num1 + bx]
+    add bx, 2
+
+    cmp bx, 6 ; when bx==6, result is 0, zf i set, now loop can stop by both jnz, and jne
+    jne outerloop ; jne "jump if not equal", jump if previous result is not equal
+
+
+  mov [num1 + bx], ax
+
+  mov ax, 0x4c00 ; exit code ..
+  int 0x21 ; .. is what the OS should do for me
+
+
+num1: dw 5, 10, 15, 0
