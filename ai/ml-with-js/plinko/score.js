6b48b0627583de1d331a6a74b68ad2d2c1f31d26
Muhammad Umer <umerkang66@gmail.com>
2024-08-28 23:58:45 +0500

    add knn algo in our app

--- ai/ml-with-js/plinko/score.js
+++ ai/ml-with-js/plinko/score.js
@@ -1,16 +1,29 @@
-const IDropPosition = 0;
-const IBounciness = 0;
-const ISize = 0;
-const IBucketLabel = 0;
-
 const outputs = [];
+const predictionPoint = 300;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  // dropPosition, bounciness, size, bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  // this is knn algo implemented from scratch
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log('Your point will probability fall into:', bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
 }
