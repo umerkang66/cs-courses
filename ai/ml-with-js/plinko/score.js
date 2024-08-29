de566a7ffd4981727079d8fbef462f0841cb874f
Muhammad Umer <umerkang66@gmail.com>
2024-08-29 19:46:32 +0500

    complete plinko

--- ai/ml-with-js/plinko/score.js
+++ ai/ml-with-js/plinko/score.js
@@ -1,6 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // dropPosition, bounciness, size, bucket
@@ -8,9 +6,33 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  // this is knn algo implemented from scratch
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 100;
+  // we have seen that the k=10, is the giving us the relatively more accurate results
+  const k = 10;
+
+  // TESTING WITH DIFFERENT K
+  _.range(0, 3).forEach(feature => {
+    const data = _.map(outputs, row => [row[feature], _.last(row)]);
+
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+
+    const accuracy = _.chain(testSet)
+      .filter(
+        testPoint =>
+          knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log('For Feature:', feature, ', Accuracy:', accuracy);
+  });
+}
+
+function knn(data, predictionPoint, k) {
+  // predictionPoint is [300, .5, 16], it has all the metrics, except the bucket, which we will probability don't know when giving our own data for prediction (it will be known in the test data)
+  return _.chain(data)
+    .map(row => [distance(_.initial(row), predictionPoint), _.last(row)])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -20,10 +42,42 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log('Your point will probability fall into:', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  // pointA=[300, .5, 16] pointB=[350, .55, 16]
+  // zip will do this
+  // [[300, 350], [.5, .55], [16, 16]]
+  // n-dimensional pythagoras theorem
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  // featureCount: how many columns we want to normalize
+  const clonedData = _.cloneDeep(data);
+  for (let j = 0; j < featureCount; j++) {
+    const column = clonedData.map(row => row[j]);
+    const min = _.min(column);
+    const max = _.max(column);
+    for (i = 0; i < clonedData.length; i++) {
+      // min-max formula
+      clonedData[i][j] = (clonedData[i][j] - min) / (max - min);
+    }
+  }
+  return clonedData;
 }
