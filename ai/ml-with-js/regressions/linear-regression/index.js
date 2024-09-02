eea2ad824ae3dccfa9d151bc5da36152742e1067
Muhammad Umer <umerkang66@gmail.com>
2024-09-02 15:28:13 +0500

    add logistic regression

--- ai/ml-with-js/regressions/linear-regression/index.js
+++ ai/ml-with-js/regressions/linear-regression/index.js
@@ -5,15 +5,18 @@ process.env['TF_CPP_MIN_LOG_LEVEL'] = '2';
 require('@tensorflow/tfjs-node');
 const plot = require('node-remote-plot');
 
-const loadCSV = require('./load-csv');
+const loadCSV = require('../load-csv');
 const LinearRegression = require('./linear-regression');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
-  shuffle: true,
-  splitTest: 50,
-  dataColumns: ['horsepower', 'weight', 'displacement'],
-  labelColumns: ['mpg'],
-});
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  '../data/cars.csv',
+  {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower', 'weight', 'displacement'],
+    labelColumns: ['mpg'],
+  }
+);
 
 const regression = new LinearRegression(features, labels, {
   // this will be adjusted automatically
