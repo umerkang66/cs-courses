09fa6c3910b81a9ba6311f64ae0345bcbeb75715
Muhammad Umer <umerkang66@gmail.com>
2024-12-05 20:19:30 +0500

    add notes for heap

--- dsa/interview-prep/heap/main.cpp
+++ dsa/interview-prep/heap/main.cpp
@@ -1,8 +1,21 @@
 // imagine as complete binary tree, but create
 // this data structure with the help of vectors
 
+// these properties works for complete binary trees
+// left child = 2*i+1
+// right child = 2*i+2
+// parent = (i-1)/2
+
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Heap
+{
+  vector<int> c_binary_tree;
+
+public:
+};
 
 int main()
 {
