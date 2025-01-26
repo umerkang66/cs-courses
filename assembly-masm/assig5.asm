b14d95f93151a0d8c8064e197729ee2618c2e4ef
Muhammad Umer <umerkang66@gmail.com>
2025-01-26 15:42:26 +0500

    almost completed assignment 5 of coal

--- assembly-masm/assig5.asm
+++ assembly-masm/assig5.asm
@@ -1,13 +1,15 @@
 .model small
 
 data_seg segment 'data'
-  st1 db "Enter a 3 digit Num: $"
-  st2 db 13, 10, "Enter a 1 digit Num: $"
+  st1 db "Enter a 3 digit1 Num: $"
+  st2 db 13, 10, "Enter a 3 digit2 Num: $"
   st3 db 13, 10, "Operation +, -, *, / : $"
   st4 db 13, 10, "Result: $"
-  multiplicant db 0
+  digit1hex db 0
+  digit2hex db 0
   digit1 db 50 DUP (?)
-  digit2 db 1 DUP (?)
+  digit2 db 50 DUP (?)
+  result db '000$'
   operation db 1 DUP (?)
 data_seg ends
 
@@ -37,11 +39,43 @@ main proc far
   mov bx, 4
   mov dh, 1
   mov cx, 3
-  loop_body:
+  loop_body2:
     sub digit1[bx], 30h
     mov al, digit1[bx]
     mul dh
-    add multiplicant, al
+    add digit1hex, al
+    mov al, dh
+    mov dl, 10
+    mul dl
+    mov dh, al
+    dec bx
+    dec cx
+    cmp cx, 0
+    jg loop_body2
+  
+  ; now digit1hex has the 3 digit number in integer hex form
+
+  mov ah, 09h
+  mov dx, offset st2
+  int 21h
+
+  mov digit2[0], 4
+  mov dx, offset digit2
+  mov ah, 0ah
+  int 21h
+  mov bl, digit2[1]; bx = count
+  mov bh, 00h
+  mov digit2[bx+2], '$'
+
+  ; convert three digit number into one number in register
+  mov bx, 4
+  mov dh, 1
+  mov cx, 3
+  loop_body:
+    sub digit2[bx], 30h
+    mov al, digit2[bx]
+    mul dh
+    add digit2hex, al
     mov al, dh
     mov dl, 10
     mul dl
@@ -51,16 +85,7 @@ main proc far
     cmp cx, 0
     jg loop_body
   
-  ; now multiplicant has the 3 digit number in integer hex form
-
-  mov ah, 09h
-  mov dx, offset st2
-  int 21h
-
-  mov ah, 01h
-  int 21h
-  mov digit2, al
-
+  ; now digit2hex has the 3 digit number in integer hex form
   mov ah, 09h
   mov dx, offset st3
   int 21h
@@ -69,10 +94,72 @@ main proc far
   int 21h
   mov operation, al
 
+  cmp operation, '+'
+  je plus_operation
+  cmp operation, '-'
+  je minus_operation
+  cmp operation, '*'
+  je multiply_operation
+  ; if none of the above
+  jmp finish
+
+  plus_operation:
+    mov al, digit1hex  ; Load first number
+    add al, digit2hex  ; Add second number
+    mov bl, al         ; Save the result for further processing
+    mov bh, 0
+    push bx
+    call display_decimal
+    jmp finish
+  minus_operation:
+    mov al, digit1hex  ; Load first number
+    sub al, digit2hex  ; Sub second number
+    mov bl, al         ; Save the result for further processing
+    mov bh, 0
+    push bx
+    call display_decimal
+    jmp finish
+  multiply_operation:
+    mov al, digit1hex  ; Load first number
+    mul digit2hex      ; Multiply second number
+    push ax
+    call display_decimal
+    jmp finish
+
+  finish:
   mov ah, 04ch
   int 21h
 
 main endp
+  ; this receives 1 argument in stack means 2 bytes
+display_decimal proc near 
+  mov bp, sp
+  mov ax, [bp + 2]
+
+  ; Convert result (in BL) to decimal
+  mov cx, 3          ; We expect 3 digits
+  lea di, result+2   ; Point DI to the end of the result buffer (reserve space for 3 digits)
+  convert_to_decimal:
+  mov dx, 0      ; Clear DX for division
+  mov dl, 10     ; Divisor is 10
+  div dl         ; Divide AX by 10, quotient in AL, remainder in AH
+  add ah, '0'    ; Convert remainder to ASCII
+  mov [di], ah   ; Store ASCII digit
+  dec di         ; Move to the previous digit position
+  ; Set AX to quotient, al already has quotient
+  mov ah, 00h
+  loop convert_to_decimal ; Repeat for next digit
+
+  ; Display the result
+  lea dx, st4        ; Load "Result: " string
+  mov ah, 09h
+  int 21h
+
+  lea dx, result     ; Point to the start of the result buffer
+  mov ah, 09h
+  int 21h
+ret 2
+display_decimal endp
 code_seg ends
 
 end main
